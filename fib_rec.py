0b296e504ef3c52d973e2115d9737329266cbea1
Jomon Vincent <jomonvincent6@gmail.com>
2025-07-02 20:51:55 +0530

    fibonnaci program and some pattens

---
+++ fib_rec.py
@@ -0,0 +1,27 @@
+def fib_normal(n):
+    fib=[]
+    if n>=1:
+        a=0
+        fib.append(a)
+    if n>=2:
+        b=1
+        fib.append(b)
+    if(n>2):
+        while(n-2):
+            c=a+b
+            fib.append(c)
+            a,b=b,c
+            n-=1
+    return fib
+print(fib_normal(3))
+
+
+
+
+def fibonacci(n):
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[i-1] + fib[i-2])
+    return fib
+
+print(fibonacci(10))
---
+++ pattern.py
@@ -0,0 +1,11 @@
+n=10
+for i in range(n,0,-1):
+    for j in range(i,n):
+        print(j,end="")
+    print()
+
+for i in range(0,n,1):
+    for j in range(i,n):
+        print(j,end="")
+    print()
+

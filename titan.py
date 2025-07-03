e14613d03c76dbec12b61048642c38b7c98e8e07
Student <Student@ASIET.Local>
2025-07-03 16:03:06 +0530

    some leetcode problem

---
+++ titan.py
@@ -0,0 +1,11 @@
+#we need to find the number of iteration it will take to kill the titan
+d=200           #health of the titan
+m=5             #no of squads
+ad=[10,20,30,20,10] #damage of every squad
+rt=[1,2,5,2,1]
+na=[0]*len(ad)
+cd=0
+tc=0
+while d>0:
+    if tc
+    
